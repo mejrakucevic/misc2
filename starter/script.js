4f0e88c474d15fb57224d3c664dc2e7efb7aac30
mejrakucevic <mejrakucevic420@gmail.com>
2023-01-02 20:09:36 +0100

    .

--- starter/script.js
+++ starter/script.js
@@ -218,10 +218,9 @@
 
 // console.log(jonas.challenge());
 
-
 // 46 for
 
-// for loop keeps running while condition is TRUE
+// for loop keeps running while condition is TRUE!
 for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep} `)
 }

032b96ec24210f971024f09baa23e23262f77bd1
mejrakucevic <mejrakucevic420@gmail.com>
2023-01-03 18:20:40 +0100

    .

--- assignments.js
+++ assignments.js
@@ -33,22 +33,30 @@
 //     neighbours : ['Hungary', 'Romania', 'Bulgaria', 'Macedonia', 'Albania', 'Montenegro', 'Bosnia N Hercegovina', 'Croatia']
 // }
 
-function percentageOfWorld1(population) {
-        return (population / 7900) * 100;
+// function percentageOfWorld1(population) {
+//         return (population / 7900) * 100;
 
-        const percPortugal1 = percentageOfWorld1(10);
-        const percChina1 = percentageOfWorld1(1441);
-        const percUSA1 = percentageOfWorld1(332);
-    }
+//         const percPortugal1 = percentageOfWorld1(10);
+//         const percChina1 = percentageOfWorld1(1441);
+//         const percUSA1 = percentageOfWorld1(332);
+//     }
 
 
-let percentages2 = [];
-const populations = [10, 1441, 332, 63];
+// let percentages2 = [];
+// const populations = [10, 1441, 332, 63];
 
-for (let i = 0; i <= populations.length; i++) {
-    const perc = percentageOfWorld1([populations[i]]);
+// for (let i = 0; i <= populations.length; i++) {
+//     const perc = percentageOfWorld1([populations[i]]);
 
-    percentages2.push(perc);
-}
+//     percentages2.push(perc);
+// }
 
-console.log(percentages2);
+// console.log(percentages2);
+
+
+// listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 
+// 'Russia']];
+
+// for (let i = 0; i < listOfNeighbours.length; i++) { 
+//     for (let y = 0; y < listOfNeighbours[i].length; y++)
+//         console.log(`Neighbour : ${listOfNeighbours[i] [y]}`) }
--- starter/script.js
+++ starter/script.js
@@ -265,23 +265,24 @@
 // };
 
 // 48
-const jonas = [
-        'Jonas',
-        'Schmedtmann',
-        2022 - 1991,
-        46,
-        ['Michael', 'Peter']
-];
+// const jonas = [
+//         'Jonas',
+//         'Schmedtmann',
+//         2022 - 1991,
+//         46,
+//         ['Michael', 'Peter']
+// ];
 
-// backwards loop 4,3,2,1,0
-for (let i = jonas.length - 1; i >=0; i--){
-    console.log(i, jonas[i]);
-}
+// // backwards loop 4,3,2,1,0
+// for (let i = jonas.length - 1; i >=0; i--){
+//     console.log(i, jonas[i]);
+// }
 
-for (let exercise = 1; exercise <= 3; exercise++) {
-    console.log(`------------Starting exercise ${exercise}------------`);
+// for (let exercise = 1; exercise <= 3; exercise++) {
+//     console.log(`------------Starting exercise ${exercise}------------`);
 
-    for (let rep = 1; rep <=5; rep++) {
-        console.log(`Lifting weight repetition ${rep}`)
-    }
-}
+//     for (let rep = 1; rep <=5; rep++) {
+//         console.log(`Lifting weight repetition ${rep}`)
+//     }
+// }
+

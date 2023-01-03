cb272d3fcbc08ff8da57a3167726981d89e1ba8e
mejrakucevic <mejrakucevic420@gmail.com>
2023-01-03 18:29:30 +0100

    .

--- starter/script.js
+++ starter/script.js
@@ -286,3 +286,23 @@
 //     }
 // }
 
+
+// 49 WHILE loops
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} `)
+// };
+
+
+// let rep = 1;
+// while (rep<=10) {
+//     console.log(`Lifting weights repetition ${rep} `)
+//     rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log("loop is about to end...")
+}

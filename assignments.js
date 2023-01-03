38791854df06cbc3aec0058f529ced2908f8d414
mejrakucevic <mejrakucevic420@gmail.com>
2023-01-03 17:57:47 +0100

    .

--- assignments.js
+++ assignments.js
@@ -25,10 +25,30 @@
 
 // 43 objects
 
-const myCountry = {
-    country : 'Serbia',
-    capital : 'Belgrade',
-    language : 'Serbian',
-    population : 6.6,
-    neighbours : ['Hungary', 'Romania', 'Bulgaria', 'Macedonia', 'Albania', 'Montenegro', 'Bosnia N Hercegovina', 'Croatia']
+// const myCountry = {
+//     country : 'Serbia',
+//     capital : 'Belgrade',
+//     language : 'Serbian',
+//     population : 6.6,
+//     neighbours : ['Hungary', 'Romania', 'Bulgaria', 'Macedonia', 'Albania', 'Montenegro', 'Bosnia N Hercegovina', 'Croatia']
+// }
+
+function percentageOfWorld1(population) {
+        return (population / 7900) * 100;
+
+        const percPortugal1 = percentageOfWorld1(10);
+        const percChina1 = percentageOfWorld1(1441);
+        const percUSA1 = percentageOfWorld1(332);
+    }
+
+
+let percentages2 = [];
+const populations = [10, 1441, 332, 63];
+
+for (let i = 0; i <= populations.length; i++) {
+    const perc = percentageOfWorld1([populations[i]]);
+
+    percentages2.push(perc);
 }
+
+console.log(percentages2);
--- starter/script.js
+++ starter/script.js
@@ -221,6 +221,45 @@
 // 46 for
 
 // for loop keeps running while condition is TRUE!
-for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep} `)
-}
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} `)
+// }
+
+// 47
+const jonas = [
+        'Jonas',
+        'Schmedtmann',
+        2022 - 1991,
+        46,
+        ['Michael', 'Peter']
+];
+
+// const types = [];
+
+// for (let i = 0; i < jonas.length; i++) {
+//     // reading from jonas array
+//     console.log(jonas[i])
+
+//     // filling types array
+//     // types[i] = typeof jonas[i];
+//     types.push(typeof jonas[i])
+// };
+
+// console.log(types)
+
+// const years = [1991, 2007, 1969, 2020]
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2023 - years[i]);
+// }
+
+// console.log(ages);
+
+// continue and break
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+    // if (typeof jonas[i] !== 'string') continue; // continue is basically skip
+
+    console.log(jonas[i])
+};
